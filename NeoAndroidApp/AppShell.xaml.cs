15a6c5630a26e0d1df7509312a81d9b3e1b058a4
mxdnxghts <cloudyfg@gmail.com>
2026-02-12 06:47:50 +0300

    [NeoApp] Add mobile app

---
+++ NeoAndroidApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NeoAndroidApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ NeoAndroidApp/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Tesseract;
+using IronOcr;
+
+namespace NeoAndroidApp;
+
+public partial class MainPage : ContentPage
+{
+    private TesseractEngine _engine;
+
+    private readonly IronTesseract _ocrTesseract;
+
+    public MainPage()
+	{
+		InitializeComponent();
+        _ocrTesseract = new IronTesseract()
+        {
+            Language = OcrLanguage.Financial,
+        };
+	}
+
+    public async Task InitializeAsync()
+    {
+        //var tessDataPath = Path.Combine(
+        //    FileSystem.Current.AppDataDirectory,
+        //    "TessData"
+        //);
+
+        //_engine = new TesseractEngine(
+        //    datapath: tessDataPath,
+        //    language: "eng+math",
+        //    engineMode: EngineMode.LstmOnly
+        //);
+    }
+
+    private async void ReadFileOnImport(object? sender, EventArgs e)
+	{
+		var picker = await MediaPicker.PickPhotosAsync();
+		var fullPath = picker[0].FullPath;
+        using var stream = await picker[0].OpenReadAsync();
+        using var ocrInput = new OcrInput();
+        ocrInput.LoadImage(stream);
+
+        var ocrResult = await _ocrTesseract.ReadAsync(ocrInput);
+
+        SemanticScreenReader.Announce(ocrResult.Text);
+	}
+}
